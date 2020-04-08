13d2a8ca4aa39cb120c19451eb65d09c2ee0cd52
lucasalmeidadecamargo@hotmail.com <lucasalmeidadecamargo@hotmail.com>
2020-04-08 15:32:24 -0300

    #cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const { uuid } = require("uuidv4");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const repositories = [];
